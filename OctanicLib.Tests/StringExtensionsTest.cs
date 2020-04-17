80762034096f346c6a88dddf6ed0eb843c757fde
João Luis de Bem Fernandes <joaoluisbf@hotmail.com>
2020-04-17 18:48:51 -0300

    Add project files.

---
+++ OctanicLib.Tests/StringExtensionsTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OctanicLib;
+using System;
+
+namespace OctanicLib.Tests
+{
+    [TestClass]
+    public class StringExtensionsTest
+    {
+        [TestMethod]
+        public void RemoveAccentsValidWord()
+        {
+            var test = "João almoço ROLÊ ROLÉ ROLÈ";
+            Assert.AreEqual(test.RemoveAccents(), "Joao almoco ROLE ROLE ROLE");
+        }
+        
+        [TestMethod]
+        public void RemoveAccentsWithoutReallyNeedIt()
+        {
+            var test = "the quick brown fox";
+            Assert.AreEqual(test.RemoveAccents(), test);
+        }
+
+        [TestMethod]
+        public void RemoveAccentsFromNothing()
+        {
+            string test = null;
+            Assert.ThrowsException<NullReferenceException>(()=>{ test.RemoveAccents(); });
+        }
+
+        [TestMethod]
+        public void RemoveAccentsFromEmpty()
+        {
+            string test = string.Empty ;
+            Assert.IsTrue(string.IsNullOrEmpty(test.RemoveAccents()));
+
+        }
+        [TestMethod]
+        public void TrySomeWeirdCharacters()
+        {
+            string test = "今一和、友達ーくん。";
+            Assert.AreEqual(test, test.RemoveAccents());
+
+        }
+    }
+}
---
+++ OctanicLib/StringExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace OctanicLib
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Remove accents from a string
+        /// </summary>
+        /// <param name="text">String to apply</param>
+        /// <returns>Another <see cref="string">string</see> without accents</returns>
+        public static string RemoveAccents(this string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
